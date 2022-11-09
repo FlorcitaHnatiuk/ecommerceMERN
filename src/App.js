d76dca9a2e505c9329f6da748338f9e73a9bb603
Florcita Hnatiuk <flopi.hnatiuk@gmail.com>
2022-11-09 17:14:40 -0300

    starting proyect

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">Ecommerce</a>
+      </header>
+      <main>
+        List of products
+      </main>
+    </div>
+  );
+}
+
+export default App;
