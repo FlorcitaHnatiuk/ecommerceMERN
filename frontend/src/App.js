e1d8c48436e5a928c6c5d80dba1cb8040863bb9b
Florcita Hnatiuk <flopi.hnatiuk@gmail.com>
2022-11-09 19:24:56 -0300

    Adding backend, still not working at HomeScreen because I cant make it to consume data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,24 @@
 
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen'
+import ProductScreen from './screens/ProductScreen'
+import { Link } from 'react-router-dom';
+
 function App() {
   return (
-    <div>
-      <header>
-        <a href="/">Ecommerce</a>
-      </header>
-      <main>
-        List of products
-      </main>
-    </div>
+    <BrowserRouter>
+      <div>
+        <header>
+          <Link to="/">Ecommerce</Link>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/product/:slug" element={<ProductScreen />} />
+            <Route path="/" element={<HomeScreen />} />
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
 
