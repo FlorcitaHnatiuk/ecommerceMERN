a234a8a9af358cbc4a99fe57b59a9e206941e382
Florcita Hnatiuk <flopi.hnatiuk@gmail.com>
2022-11-09 22:15:49 -0300

    Error 431

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,30 @@
-
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import HomeScreen from './screens/HomeScreen'
-import ProductScreen from './screens/ProductScreen'
-import { Link } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import { LinkContainer } from "react-router-bootstrap";
 
 function App() {
   return (
     <BrowserRouter>
       <div>
         <header>
-          <Link to="/">Ecommerce</Link>
+          <Navbar bg="dark" variant="dark">
+            <Container>
+              <LinkContainer to="/">
+                <Navbar.Brand>Ecommerce</Navbar.Brand>
+              </LinkContainer>
+            </Container>
+          </Navbar>
         </header>
         <main>
+        <Container>
           <Routes>
             <Route path="/product/:slug" element={<ProductScreen />} />
             <Route path="/" element={<HomeScreen />} />
           </Routes>
+          </Container>
         </main>
       </div>
     </BrowserRouter>
