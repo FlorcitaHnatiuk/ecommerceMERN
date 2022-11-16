9d781a9ec375e0f8d750fc89c4553f205e107ad0
Florcita Hnatiuk <flopi.hnatiuk@gmail.com>
2022-11-16 13:59:56 -0300

    created shipping address screen

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,30 +1,30 @@
-import express from 'express';
-import User from '../models/userModel.js';
-import bcrypt from 'bcryptjs';
-import { generateToken } from '../utils.js';
-import expressAsyncHandler from 'express-async-handler';
+import express from "express";
+import User from "../models/userModel.js";
+import bcrypt from "bcryptjs";
+import { generateToken } from "../utils.js";
+import expressAsyncHandler from "express-async-handler";
 
 const userRouter = express.Router();
 
 userRouter.post(
-    // this function catches the error at the async function
-    '/signin', 
-    expressAsyncHandler(async (req, res) => {
-        const user = await User.findOne({ email: req.body.email });
-        if (user) {
-            if (bcrypt.compareSync(req.body.password, user.password)) {
-                res.send({
-                    _id: user._id,
-                    name: user.name,
-                    email: user.email,
-                    isAdmin: user.isAdmin,
-                    token: generateToken(user),
-                });
-                return;
-            }
-        }
-        res.status(401).send({message: 'Invalid username or password'});
-    })
+  // this function catches the error at the async function
+  "/signin",
+  expressAsyncHandler(async (req, res) => {
+    const user = await User.findOne({ email: req.body.email });
+    if (user) {
+      if (bcrypt.compareSync(req.body.password, user.password)) {
+        res.send({
+          _id: user._id,
+          name: user.name,
+          email: user.email,
+          isAdmin: user.isAdmin,
+          token: generateToken(user),
+        });
+        return;
+      }
+    }
+    res.status(401).send({ message: "Invalid username or password" });
+  })
 );
 
 export default userRouter;
