c198234777242abeccb9eb3ef0c4ed182371bd2c
Florcita Hnatiuk <flopi.hnatiuk@gmail.com>
2022-12-14 16:30:29 -0300

    created delete button for products

--- frontend/src/screens/ProductListScreen.js
+++ frontend/src/screens/ProductListScreen.js
@@ -33,17 +33,39 @@ const reducer = (state, action) => {
             };
         case 'CREATE_FAIL':
             return { ...state, loadingCreate: false };
+        case 'DELETE_REQUEST':
+            return { ...state, loadingDelete: true, successDelete: false };
+        case 'DELETE_SUCCESS':
+            return {
+                ...state,
+                loadingDelete: false,
+                successDelete: true,
+            };
+        case 'DELETE_FAIL':
+            return { ...state, loadingDelete: false, successDelete: false };
+        case 'DELETE_RESET':
+            return { ...state, loadingDelete: false, successDelete: false };
         default:
             return state;
     }
 };
 
 export default function ProductListScreen() {
-    const [{ loading, error, products, pages, loadingCreate }, dispatch] =
-        useReducer(reducer, {
-            loading: true,
-            error: '',
-        });
+    const [
+        {
+            loading,
+            error,
+            products,
+            pages,
+            loadingCreate,
+            loadingDelete,
+            successDelete,
+        },
+        dispatch,
+    ] = useReducer(reducer, {
+        loading: true,
+        error: '',
+    });
 
     const navigate = useNavigate();
     const { search } = useLocation();
@@ -59,12 +81,15 @@ export default function ProductListScreen() {
                 const { data } = await axios.get(`/api/products/admin?page=${page} `, {
                     headers: { Authorization: `Bearer ${userInfo.token}` },
                 });
-
                 dispatch({ type: 'FETCH_SUCCESS', payload: data });
             } catch (err) { }
         };
-        fetchData();
-    }, [page, userInfo]);
+        if (successDelete) {
+            dispatch({ type: 'DELETE_RESET' });
+        } else {
+            fetchData();
+        }
+    }, [page, userInfo, successDelete]);
 
     const createHandler = async () => {
         if (window.confirm('Are you sure to create?')) {
@@ -88,13 +113,31 @@ export default function ProductListScreen() {
             }
         }
     };
+
+    const deleteHandler = async (product) => {
+        if (window.confirm('Are you sure to delete?')) {
+            try {
+                await axios.delete(`/api/products/${product._id}`, {
+                    headers: { Authorization: `Bearer ${userInfo.token}` },
+                });
+                toast.success('product deleted successfully');
+                dispatch({ type: 'DELETE_SUCCESS' });
+            } catch (err) {
+                toast.error(getError(error));
+                dispatch({
+                    type: 'DELETE_FAIL',
+                });
+            }
+        }
+    };
+
     return (
         <div>
             <Row>
                 <Col>
-                    <h1 className="text-center my-5">Products</h1>
+                    <h1>Products</h1>
                 </Col>
-                <Col className="col text-end my-5">
+                <Col className="col text-end">
                     <div>
                         <Button type="button" onClick={createHandler}>
                             Create Product
@@ -104,6 +147,7 @@ export default function ProductListScreen() {
             </Row>
 
             {loadingCreate && <LoadingBox></LoadingBox>}
+            {loadingDelete && <LoadingBox></LoadingBox>}
 
             {loading ? (
                 <LoadingBox></LoadingBox>
@@ -137,6 +181,14 @@ export default function ProductListScreen() {
                                             onClick={() => navigate(`/admin/product/${product._id}`)}
                                         >
                                             Edit
+                                        </Button>
+                                        &nbsp;
+                                        <Button
+                                            type="button"
+                                            variant="light"
+                                            onClick={() => deleteHandler(product)}
+                                        >
+                                            Delete
                                         </Button>
                                     </td>
                                 </tr>
