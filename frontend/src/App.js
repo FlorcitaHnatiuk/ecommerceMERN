a8306d8f35f4061ca4b69b045b43bbf2bea2471d
Florcita Hnatiuk <flopi.hnatiuk@gmail.com>
2022-12-19 15:16:49 -0300

    created order list screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -31,6 +31,7 @@ import AdminRoute from './components/AdminRoute';
 import { SocialIcon } from 'react-social-icons';
 import ProductListScreen from './screens/ProductListScreen';
 import ProductEditScreen from './screens/ProductEditScreen';
+import OrderListScreen from './screens/OrderListScreen';
 
 function App() {
   const { state, dispatch: ctxDispatch } = useContext(Store);
@@ -205,6 +206,14 @@ function App() {
                   </AdminRoute>
                 }
               ></Route>
+              <Route
+              path="/admin/orders"
+              element={
+                <AdminRoute>
+                  <OrderListScreen />
+                </AdminRoute>
+              }
+            ></Route>
               <Route
               path="/admin/products"
               element={
