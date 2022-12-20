6b282adffa3f01c9ccff1b10dbdcad1584be10ad
Florcita Hnatiuk <flopi.hnatiuk@gmail.com>
2022-12-19 21:31:51 -0300

    cannot display data at info route

--- frontend/src/screens/InfoScreen.js
+++ frontend/src/screens/InfoScreen.js
@@ -1,6 +1,5 @@
-import React, { useContext, useEffect, useReducer }  from 'react'
+import React from 'react'
 import { Helmet } from 'react-helmet-async';
-import axios from 'axios';
 
 export default function InfoScreen() {
     return (
@@ -10,14 +9,13 @@ export default function InfoScreen() {
             </Helmet>
             <h1 className="text-center my-5">Web Information</h1>
             <section class="alert alert-primary">
-            <p><b>Port: {port}</b></p>
-            <p><b>Ops: {system}</b></p>
-            <p><b>Node Version: {nodeVersion}</b></p>
-            <p><b>Memory Usage: {memory}</b></p>
-            <p><b>Path: {path}</b></p>
-            <p><b>Process id: {processId}</b></p>
-            <p><b>Title: {title}</b></p>
-            <p><b>File Path: {filePath}</b></p>
+            <p><b>Port: </b></p>
+            <p><b>Ops: </b></p>
+            <p><b>Node Version: </b></p>
+            <p><b>Memory Usage: </b></p>
+            <p><b>Process id: </b></p>
+            <p><b>Title: </b></p>
+            <p><b>File Path: </b></p>
         </section>
         </div>
     )
